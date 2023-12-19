3eeafd63b0ee05356e32aa14b5ae6c0949869f8b
Nico Spiller <nspiller1@mailbox.org>
2023-12-19 15:43:54 -0500

    renamed temp folder to out

--- caiman/utils.py
+++ caiman/utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from tifffile import imread, imwrite, TiffFile
 
-def split_dual_channel_tif(p_tif, tmp_dir):
+def split_dual_channel_tif(p_tif, p_out):
     """Split scanimage dual-channel tif file into two separate files
 
     Writes new files ending with `<filename>_ch1.tif` and `<filename>_ch2.tif`
@@ -12,10 +12,12 @@ def split_dual_channel_tif(p_tif, tmp_dir):
     ----------
     p_tif : pathlike
         path to tif file
+    p_out : pathlike
+        folder to store the split tifs (e.g. tmp folder on local disk)
     """
     
     p_tif = Path(p_tif)
-    tmp_dir = Path(tmp_dir)
+    p_out = Path(p_out)
 
     tif = TiffFile(p_tif)
     n_pages = len(tif.pages)
@@ -26,8 +28,8 @@ def split_dual_channel_tif(p_tif, tmp_dir):
     ch1 = imread(p_tif, key=range(0, n_pages, 2))
     ch2 = imread(p_tif, key=range(1, n_pages, 2))
 
-    p_tif_ch1 = tmp_dir / f"{p_tif.stem}_ch1{p_tif.suffix}"
-    p_tif_ch2 = tmp_dir / f"{p_tif.stem}_ch2{p_tif.suffix}"
+    p_tif_ch1 = p_out / f"{p_tif.stem}_ch1{p_tif.suffix}"
+    p_tif_ch2 = p_out / f"{p_tif.stem}_ch2{p_tif.suffix}"
 
     imwrite(p_tif_ch1, ch1, metadata={"axes": "TYX", "fps": fps})
     imwrite(p_tif_ch2, ch2, metadata={"axes": "TYX", "fps": fps})
